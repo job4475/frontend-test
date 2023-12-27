2d21f19a73b86b29296ee65e1aeafe1de22cce39
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2023-12-27 14:41:10 +0700

    v.11

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,13 @@
 'use client'
 import { StateContext } from "@/context/Context";
 import { Button } from "@mui/material";
-import Image from "next/image";
 import { useContext } from "react";
-
+import Login from './Login/page'
 export default function Home() {
   const {state, setState} = useContext(StateContext);
   return (
     <div>
-      <h1 className="text-3xl font-bold underline mb-4">Hello world!</h1>
-      <Button>Click me</Button>
+    <Login/>
     </div>
   );
 }
