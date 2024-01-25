85d18afd8bd0c289860abfc381156cb628cc3bbe
Pond161172 <kraiwit1711@gmail.com>
2024-01-25 16:12:46 +0700

    reload button

--- app/Register/components/button/index.js
+++ app/Register/components/button/index.js
@@ -11,7 +11,7 @@ function index() {
   const { state, setState } = useContext(StateContext);
   const Handleregis = handleregis();
   return (
-    <Box sx={{ mt: '25px', display: 'flex' }}>
+    <Box sx={{ mt: '25px', display: 'flex',gap:'12px' }}>
       <Button variant="outlined" color="success" onClick={Handleregis.Selectcompany} style={{ textTransform: 'capitalize', width: '100px', height: '50px', color: '#84BAA1', borderRadius: '8px' }}>Back</Button>
       <Button variant='contained' onClick={Handleregis.handleRegister} sx={{ transition: 'transform 0.3s ease', '&:hover': { transform: 'scale(1.03)', background: '#84BAA1', boxShadow: '0px 0px 0px' }, gap: '8px', background: '#84BAA1', color: 'white', width: '150px', height: '50px', textTransform: 'capitalize', boxShadow: '0px 0px 0px', borderRadius: '8px', fontWeight: '600' }}>
         <Box sx={{ display: 'flex', gap: '12px' }}>{state.loading ? <Loading /> : "Next"}<Arrow style={{ transform: 'rotate(90deg)' }} /></Box>
