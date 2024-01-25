2b6551f4ec0ff7132e7812f224146365accd47ac
wawayayoei1234 <you@example.com>
2024-01-25 11:49:25 +0700

    enter for login

--- app/ui/login/LoginForm/LoginForm.jsx
+++ app/ui/login/LoginForm/LoginForm.jsx
@@ -11,6 +11,12 @@ import Backdrop from '@/components/backdrop/backdrop'
 function Index() {
   const {state, setState} = useContext(StateContext);
  const HandleLogin = handlelogin();
+ const handleEnterKeyPress = (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    state.info === 0 ? HandleLogin.handleSignInClick() : HandleLogin.handleSignUpClick();
+  }
+};
   return (
     <Box>
       <Box p={3} sx={{display: 'flex',flexDirection: 'column',background: 'width',width: '440px',height: '550px',
@@ -29,9 +35,9 @@ function Index() {
           </Box>
         </Box>
         <Box sx={{display: 'flex',flexDirection: 'column',justifyContent: 'center',alignItems: 'center'}}>
-          <TextField id="standard-basic" label="Email" variant="standard" value={state.Email} onChange={HandleLogin.Email} sx={{ width: '90%','& label':{color:'#828895',fontWeight:'500'}}} />
+          <TextField id="standard-basic" label="Email" variant="standard" value={state.email} onChange={HandleLogin.Email}onKeyDown={handleEnterKeyPress} sx={{ width: '90%','& label':{color:'#828895',fontWeight:'500'}}} />
           {state.info === 0 && (
-            <TextField label="Password"variant="standard" value={state.Password} onChange={HandleLogin.Password} type={state.showPassword ? 'text' : 'password'}sx={{ width: '90%',mt:'15px','& label':{color:'#828895',fontWeight:'500'}}}
+            <TextField label="Password"variant="standard" value={state.password} onChange={HandleLogin.Password}onKeyDown={handleEnterKeyPress} type={state.showPassword ? 'text' : 'password'}sx={{ width: '90%',mt:'15px','& label':{color:'#828895',fontWeight:'500'}}}
               InputProps={{
                 endAdornment: (
                      <IconButton onClick={HandleLogin.handleTogglePassword} edge="end">
