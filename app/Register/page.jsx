251e8415349da8e1e2e9d09e69fe0fd0ed83c7e0
Pond161172 <kraiwit1711@gmail.com>
2024-01-26 09:24:58 +0700

    change button Register and Creat company page

--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -9,23 +9,23 @@ import ContentPersonal from './components/content_personal'
 import Button from './components/button'
 import Map from './components/map'
 function page() {
-  const {state, setState} = useContext(StateContext);
+  const { state, setState } = useContext(StateContext);
   return (
-    <Box sx={{ background: `linear-gradient(108deg, #84BAA1 0%, #FFFBE2 100%)`,  height: "auto" }}>
-  <Grid container >
-    <Grid item >
-      <Box sx={{  display: { md: 'flex', xs: 'block' }, alignItems: "center", p: 5, height: "auto",width:"85%", background: "#fff", borderRadius: "0px 14px 14px 0px" }}>
-        <Box pt >
-          <ContentAccount />
-          <ContentCompany />
-          <ContentPersonal />
-          <Button />
-        </Box>
-        <Map/>
-      </Box>
-    </Grid>
-  </Grid>
-</Box>
+    <Box sx={{ background: `linear-gradient(108deg, #84BAA1 0%, #FFFBE2 100%)`, height: "auto" }}>
+      <Grid container >
+        <Grid item >
+          <Box sx={{ display: { md: 'flex', xs: 'block' }, alignItems: "center", p: 5, height: "auto", width: "85%", background: "#fff", borderRadius: "0px 14px 14px 0px" }}>
+            <Box pt >
+              <ContentAccount />
+              <ContentCompany />
+              <ContentPersonal />
+              <Button />
+            </Box>
+            <Map />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
