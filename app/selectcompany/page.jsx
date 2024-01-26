5a9cd95b9013b614022355f69a78d774a93389e2
Pond161172 <kraiwit1711@gmail.com>
2024-01-26 08:57:55 +0700

    hover Selectcompany

--- app/selectcompany/page.jsx
+++ app/selectcompany/page.jsx
@@ -4,23 +4,23 @@ import Image from "next/image";
 import { Box } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { StateContext } from "@/context/Context";
-import Backdrop from '@/components/backdrop/backdrop' 
+import Backdrop from '@/components/backdrop/backdrop'
 
 const page = () => {
-  
+
   const router = useRouter();
   const { state, setState } = useContext(StateContext);
   const Register = () => {
-     setState({...state,backdrop: true});
-     setTimeout(() => {
-      setState((prevData) => ({ ...prevData, backdrop: false}));
+    setState({ ...state, backdrop: true });
+    setTimeout(() => {
+      setState((prevData) => ({ ...prevData, backdrop: false }));
     }, 1000);
     router.push('/Register');
   }
   const CreateCompany = () => {
-    setState({...state,backdrop: true});
+    setState({ ...state, backdrop: true });
     setTimeout(() => {
-      setState((prevData) => ({ ...prevData, backdrop: false}));
+      setState((prevData) => ({ ...prevData, backdrop: false }));
     }, 1000);
     router.push('/CreateCompany');
   }
@@ -31,24 +31,24 @@ const page = () => {
         background: `linear-gradient(108deg, #84BAA1 0%, #FFFBE2 100%), #F7FAFB`,
       }}
     >
-      {state.backdrop?<Backdrop/>:""}
+      {state.backdrop ? <Backdrop /> : ""}
       <div className="bg-white h-screen w-4/5 p-16">
         <h2>Hi, Is this the company you work ?</h2>
         <p>Please select your company.</p>
         <div className="flex lg:flex-row flex-col items-center content-center">
-          <Box onClick={Register} sx={{cursor: "pointer",transition: "background-color 0.3s ease","&:hover": {backgroundColor: "#your-hover-color",},}}   className="basis-2/6 rounded border border-[#C2CCE1] w-[300px] h-[310px] mr-3 mt-2 lg:mt-4 flex flex-col justify-between items-center content-center p-[30px] text-center" >
-            <div>
-            <Image
-              src={state.datacompanylc?.CompanyLogo}
-              alt="logo"
-              width={100}
-              height={100}
-            />
-            </div>
+          <Box onClick={Register} sx={{ display: 'flex', justifyContent: 'space-between', cursor: "pointer", transition: "background-color 0.3s ease", "&:hover": { backgroundColor: "#f4f9f6" } }} className="basis-2/6 rounded-[10px] border border-[#C2CCE1] w-[300px] h-[370px] mr-3 mt-2 lg:mt-4 flex flex-col  items-center py-[42px] px-[40px] text-center" >
+            <Box sx={{ transition: 'transform 0.3s ease', '&:hover': { transform: 'scale(1.03)' } }}>
+              <Image
+                src={state.datacompanylc?.CompanyLogo}
+                alt="logo"
+                width={90}
+                height={90}
+              />
+            </Box>
             <div>
               <h4>{state.datacompanylc.Companyname}</h4>
             </div>
-            <div className="my-2">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="52"
@@ -65,43 +65,41 @@ const page = () => {
               </svg>
             </div>
             <div>
-              <p>
-              {state.datacompanylc.Companyname} {state.datacompanylc.Province}, {state.datacompanylc.Zipcode} {state.datacompanylc.Country}.
+              <p className="text-[12px] font-[500]">
+                {state.datacompanylc.Companyname} {state.datacompanylc.Province}, {state.datacompanylc.Zipcode} {state.datacompanylc.Country}.
               </p>
             </div>
           </Box>
 
-          <div className="basis-2/6 rounded border border-[#C2CCE1] w-[300px] h-[310px] mr-3 mt-2 lg:mt-4 flex flex-col justify-between items-center content-center p-[30px] text-center">
-            <Box onClick={CreateCompany} sx={{cursor: "pointer",transition: "background-color 0.3s ease","&:hover": {backgroundColor: "#your-hover-color",},}} className="mb-[5px] mt-[5px] lg:mb-[20px] lg:mt-[30px]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="41"
-                height="41"
-                viewBox="0 0 41 41"
-                fill="none"
-              >
-                <path
-                  d="M19.9167 3V38"
-                  stroke="#84BAA1"
-                  strokeWidth="5"
-                  strokeLinecap="round"
-                />
-                <path
-                  d="M3 20.5L38 20.5"
-                  stroke="#84BAA1"
-                  strokeWidth="5"
-                  strokeLinecap="round"
-                />
-              </svg>
+          <Box onClick={CreateCompany} sx={{ display: 'flex', justifyContent: 'space-between', cursor: "pointer", transition: "background-color 0.3s ease", "&:hover": { backgroundColor: "#f4f9f6" } }} className="basis-2/6 rounded-[10px] border border-[#C2CCE1] w-[250px] h-[370px] mr-3 mt-2 lg:mt-4 flex flex-col  items-center py-[42px] px-[40px] text-center" >
+            <Box sx={{}}>
+              <Box sx={{ width: '90px', height: '90px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="41"
+                  height="41"
+                  viewBox="0 0 41 41"
+                  fill="none"
+                >
+                  <path
+                    d="M19.9167 3V38"
+                    stroke="#84BAA1"
+                    strokeWidth="5"
+                    strokeLinecap="round"
+                  />
+                  <path
+                    d="M3 20.5L38 20.5"
+                    stroke="#84BAA1"
+                    strokeWidth="5"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </Box>
             </Box>
             <div>
-              <h4>
-                Add new your company
-                <br />
-                <br />
-              </h4>
+              <h4>Add new your company</h4>
             </div>
-            <div className="my-2">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="52"
@@ -118,9 +116,12 @@ const page = () => {
               </svg>
             </div>
             <div>
-              <p>For users who do not have a company listed in the options.</p>
+              <p className="text-[12px] font-[500]">
+                For users who do not have a company listed in the options.
+              </p>
             </div>
-          </div>
+          </Box>
+
         </div>
       </div>
     </div>
