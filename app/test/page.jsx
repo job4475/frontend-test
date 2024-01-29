0b67f8fa697086be0dea231252b314b61def8e02
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2024-01-29 10:55:24 +0700

    test fix

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 function page() {
@@ -8,23 +7,3 @@ function page() {
 }
 
 export default page
-=======
-'use client'
-import { StateContext } from '@/context/Context';
-import React, { useContext, useState } from 'react';
-import OtpInput from 'react-otp-input';
-
-export default function App() {
-  const { state, setState } = useContext(StateContext);
-  const handleChange = (value) => {
-    if (/^\d*$/.test(value)) {
-      setState({ ...state, input_OTP: value });
-    }
-  };
-  return (
-    <OtpInput value={state.input_OTP} onChange={handleChange} numInputs={6}isInputNum={true} separator={<span>-</span>}
-      renderInput={(props) => <input {...props} />} inputStyle={{ width: '2rem', height: '2rem', margin: '0 0.5rem',
-        fontSize: '1.5rem', borderBottom: '1px solid #BBC0CA',  }} />
-  );
-}
->>>>>>> aa36b1510b5182940b75a10bbbe08753120503fd
