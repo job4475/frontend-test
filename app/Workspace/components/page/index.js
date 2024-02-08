ffb7056f9a58f0df6ddbdf715fcde079696d468d
Pond161172 <kraiwit1711@gmail.com>
2024-02-08 15:20:17 +0700

    Change navbar

--- app/Workspace/components/page/index.js
+++ app/Workspace/components/page/index.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState,useContext } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Logotrac from "@/assets/assets/images/logotrac.png";
 import CarReserve from "@/assets/assets/images/workspace/CarReserve.png";
@@ -7,167 +7,79 @@ import MyOpportunity from "@/assets/assets/images/workspace/MyOpportunity.png";
 import RemoteSupport from "@/assets/assets/images/workspace/RemoteSupport.png";
 import ShareDocument from "@/assets/assets/images/workspace/ShareDocument.png";
 import UnderReview from "@/assets/assets/images/workspace/UnderReview.png";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
 import { StateContext } from "@/context/Context";
-import { Button, Box, Skeleton } from "@mui/material";
-import Backdrop from '@/components/backdrop/backdrop' 
-import { useCookies } from "react-cookie";
+import { Button, Box } from "@mui/material";
+import Backdrop from '@/components/backdrop/backdrop'
+import Navbar from "@/components/navbar/navbar";
 
-const Page = () => {
+const page = () => {
   const { state, setState } = useContext(StateContext);
-  const [ cookies,removeCookie] = useCookies(['token']);
-  
+  const router = useRouter();
+
   const sharedocumentRouter = () => {
-    setState((prevData) => ({ ...prevData, backdrop: true}));         
-    if(state.decode_token.Role==="admin"){
-      window.location.href = "/RequestList"
-    }else{
-      window.location.href = "/ShareDocument"
+    setState((prevData) => ({ ...prevData, backdrop: true }));
+    setTimeout(() => {
+      setState((prevData) => ({ ...prevData, backdrop: false }));
+    }, 2000);
+    if (state.decode_token.Role === "admin") {
+      router.push("/RequestList");
+    } else {
+      router.push("/ShareDocument");
     }
-    };
+  };
 
-  const isLocalStorageAvailable = typeof window !== 'undefined' && window.localStorage;
+  const remotesupportRouter = () => {
+    router.push("/remotesupport");
+  };
+  const myopportunityRouter = () => {
+    router.push("/myopportunity");
+  };
+  const carreserveRouter = () => {
+    router.push("/carreserve");
+  };
+  const underreviewRouter = () => {
+    router.push("/underreview");
+  };
+  const orgmbatFeature = state?.memberAuthorization?.orgmbat_feature;
 
-  const storedLoginTime = isLocalStorageAvailable ? localStorage.getItem('loginTime') : null;
-  const [loginTime] = useState(
-    storedLoginTime ? new Date(storedLoginTime) : new Date()
-  );
-
-  useEffect(() => {
-    const loginTime = isLocalStorageAvailable ? localStorage.getItem('loginTime') : null;
-    const initialLoginTime = loginTime ? new Date(loginTime) : new Date();
-    setLoginTime(initialLoginTime);
-
-    const intervalId = setInterval(() => {
-      const currentTime = new Date();
-      const elapsedTime = currentTime - initialLoginTime;
-      const formattedTime = formatElapsedTime(elapsedTime);
-      const loginPeriodElement = document.getElementById('loginPeriod');
-
-      if (loginPeriodElement) {
-        loginPeriodElement.innerText = `Login Period: ${formattedTime}`;
-      } else {
-        console.error("Element with id 'loginPeriod' not found in the DOM.");
-      }
-
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, [isLocalStorageAvailable]);
+  const [sharedocument, setSharedocument] = useState(false);
+  const [remotesupport, setRemotesupport] = useState(false);
+  const [myopportunity, setMyopportunity] = useState(false);
+  const [carreserve, setCarreserve] = useState(false);
+  const [underreview, setUnderreview] = useState(false);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setLoading(false);
-    }, 50);
-
-    return () => clearTimeout(timeoutId);
-  }, [setLoading]);
-
-  const handleclicklogout = () => {
-    localStorage.removeItem("ally-supports-cache")
-    localStorage.removeItem("decode_token")
-    localStorage.removeItem("loginTime")
-    window.location.href="/"
-  }
-  const Notallowed =()=>{
-
-  }
-
-  const handlesharedoc = ()=>{
-    state.memberAuthorization?.orgmbat_feature!=="#securedoc"||state.decode_token?.Role==="admin"?Notallowed():sharedocumentRouter()
-  }
+    if (orgmbatFeature) {
+      setSharedocument(orgmbatFeature.includes("#sharedocument"));
+      setRemotesupport(orgmbatFeature.includes("#remotesupport"));
+      setMyopportunity(orgmbatFeature.includes("#myopportunity"));
+      setCarreserve(orgmbatFeature.includes("#carreserve"));
+      setUnderreview(orgmbatFeature.includes("#underreview"));
+    }
+  }, [orgmbatFeature]);
 
 
   return (
     <>
-    <Backdrop/>
-      <div className="max-w-screen-xl p-2 lg:p-0 container mx-auto my-2 lg:my-12 flex flex-col lg:flex-row justify-start lg:justify-between items-start lg:items-center">
-      <div className=" flex flex-col lg:flex-row">
-          <div className="mr-3" style={{cursor:"pointer",}}>
-          {!loading ? (
-            <Image variant="rectangular"  src={Logotrac} alt="logo"  style={{ width: "90px", height: "90px", borderRadius: "99px" }} />
-            ) : (
-            <Skeleton animation="wave"  variant="rectangular"  src={Logotrac} alt="logo" style={{ width: "90px", height: "90px", borderRadius: "99px" }} />
-            )}
-            </div>
-          <div className="">
-            <div className="flex flex-col lg:flex-row justify-start lg:justify-between items-start lg:items-center">
-            {!loading ? (
-              <span style={{textTransform:"capitalize"}} className="text-lg font-semibold mr-1">
-              {state.decode_token.FirstnameOriginal?state.decode_token?.FirstnameOriginal:state.decode_token?.Firstname} {state.decode_token.SurnameTokenOriginal?state.decode_token?.SurnameTokenOriginal:state.decode_token?.Surname}
-              </span>
-              ) : (
-              <Skeleton animation="wave" height={30} width="200px" />
-              )}
-              <Box sx={{display:'flex',gap:'7px',ml:'10px'}}>
-              {!loading ? (
-                <Button disabled style={{background:'#E4E4E4',textTransform:'capitalize'}}>Edit Profile</Button>
-                ) : (
-                  <Skeleton width="100px" height="60px" disabled style={{background:'#E4E4E4',textTransform:'capitalize'}}/>
-                  )}
-                  {!loading ? (
-                <Button onClick={handleclicklogout} style={{background:'#F95353',color:'#fff',borderRadius:'4px',textTransform:'capitalize'}}>Logout</Button>
-                ) : (
-                  <Skeleton width="80px" height="60px" disabled style={{background:'#E4E4E4',textTransform:'capitalize'}}/>
-                  )}
-                </Box>
-            </div>
-            <div>
-            {!loading ? (
-            <span className="text-lg">{state.decode_token.JobTitleOriginal?state.decode_token?.JobTitleOriginal:state.decode_token?.Role}</span>
-            ) : (
-              <Skeleton animation="wave" height={30} width="190px" />
-              )}
-            </div>
-            <div>
-            {!loading ? (
-            <span id="loginPeriod" className="text-lg">Login Period: 00:00:00</span>
-            ) : (
-              <Skeleton animation="wave" height={30} width="180px" />
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="m-2">
-          <div>
-          {!loading ? (
-            <Image
-              src={state.decode_token.CompanyLogoOriginal?state.decode_token?.CompanyLogoOriginal:Logotrac}
-              alt="logo"
-              width={100}
-              height={100}
-            />
-            ) : (
-              <Skeleton
-              src={state.decode_token.CompanyLogoOriginal?state.decode_token?.CompanyLogoOriginal:Logotrac}
-              alt="logo"
-              width={100}
-              height={170}
-            />
-            )}
-          </div>
-        </div>
-      </div>
+      <newmenu />
+      <Navbar/>
 
       <div className="max-w-screen-xl p-2 container mx-auto my-2 lg:my-12">
         <h3 className="my-2 lg:my-5">My work space</h3>
         <div className="flex flex-col lg:flex-row">
           <div>
+            {state.memberAuthorization?.orgmbat_feature || state.leadAuthorization?.orgmbat_feature === "#securedoc" ? (
               <div
-               role="button"
-               tabIndex={0}
-               onClick={handlesharedoc}
-               onKeyDown={(e) => {
-                 if (e.key === 'Enter') {
-                  handlesharedoc();
-                 }
-               }}
+                onClick={state.decode_token?.Role === "admin" ? Notallowed : sharedocumentRouter}
                 // className="font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg cursor-pointer transition-colors duration-300 hover:bg-gray-200"
-                className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300 ${state.memberAuthorization?.orgmbat_feature!=="#securedoc"||state.decode_token?.Role==="admin"?"bg-gray-100":""}   ${state.memberAuthorization?.orgmbat_feature!=="#securedoc"||state.decode_token?.Role==="admin"?"":"cursor-pointer"} ${state.memberAuthorization?.orgmbat_feature!=="#securedoc"||state.decode_token?.Role==="admin"?"":"hover:bg-gray-200"} `}
+                className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300 ${state.decode_token?.Role === "admin" ? "bg-gray-100" : ""}   ${state.decode_token?.Role === "admin" ? "" : "cursor-pointer"} ${state.decode_token?.Role === "admin" ? "" : "hover:bg-gray-200"} `}
               >
                 <Image
                   src={ShareDocument}
                   alt="logo"
-                  style={{ width: "70px", height: "75px",filter:state.memberAuthorization?.orgmbat_feature!=="#securedoc"||state.decode_token?.Role==="admin"?"grayscale(1)":"" }}
+                  style={{ width: "70px", height: "75px", filter: state.decode_token?.Role === "admin" ? "grayscale(1)" : "" }}
                 />
                 <div className="my-3">
                   Secure
@@ -175,108 +87,89 @@ const Page = () => {
                   Doc
                 </div>
               </div>
+            ) : null}
           </div>
 
           <div>
-              <button
-              role="button"
-              tabIndex={0}
-              onClick={Notallowed}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  Notallowed();
-                }
-              }}
+            {state.memberAuthorization?.orgmbat_feature || state.leadAuthorization?.orgmbat_feature === "#securedoc" ? (
+              <div
+                onClick={Notallowed}
                 className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300  bg-gray-100 `}
               >
                 <Image
                   src={RemoteSupport}
                   alt="logo"
-                  style={{ width: "70px", height: "75px",filter:"grayscale(1)" }}
+                  style={{ width: "70px", height: "75px", filter: "grayscale(1)" }}
                 />
                 <div className="my-3">
-                Remote
+                  Remote
                   <br />
                   Support
                 </div>
-              </button>
+              </div>
+            ) : null}
           </div>
 
 
           <div>
-              <button
-              role="button"
-              tabIndex={0}
-              onClick={Notallowed}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  Notallowed();
-                }
-              }}
+            {state.memberAuthorization?.orgmbat_feature || state.leadAuthorization?.orgmbat_feature === "#securedoc" ? (
+              <div
+                onClick={Notallowed}
                 className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300  bg-gray-100 `}              >
                 <Image
                   src={MyOpportunity}
                   alt="logo"
-                  style={{ width: "70px", height: "75px",filter:"grayscale(1)" }}/>
+                  style={{ width: "70px", height: "75px", filter: "grayscale(1)" }} />
                 <div className="my-3">
                   My
                   <br />
                   Opportunity
                 </div>
-              </button>
+              </div>
+            ) : null}
           </div>
 
           <div>
-              <button
-              role="button"
-              tabIndex={0}
-              onClick={Notallowed}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  Notallowed();
-                }
-              }}
-              className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300  bg-gray-100 `}              >
+            {state.memberAuthorization?.orgmbat_feature || state.leadAuthorization?.orgmbat_feature === "#securedoc" ? (
+              <div
+                onClick={Notallowed}
+                className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300  bg-gray-100 `}              >
                 <Image
                   src={CarReserve}
                   alt="logo"
-                  style={{ width: "70px", height: "75px",filter:"grayscale(1)" }}/>
+                  style={{ width: "70px", height: "75px", filter: "grayscale(1)" }} />
                 <div className="my-3">
                   Car
                   <br />
                   Reserve
                 </div>
-              </button>
+              </div>
+            ) : null}
           </div>
 
           <div>
+            {state.memberAuthorization?.orgmbat_feature || state.leadAuthorization?.orgmbat_feature === "#securedoc" ? (
               <div
-              role="button"
-              tabIndex={0}
-              onClick={sharedocumentRouter}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  sharedocumentRouter();
-                }
-              }}
-                className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300 ${(state.memberAuthorization?.orgmbat_feature||state.leadAuthorization?.orgmbat_feature!=="#securedoc")||state.decode_token?.Role==="user"?"bg-gray-100":""}   ${(state.memberAuthorization?.orgmbat_feature||state.leadAuthorization?.orgmbat_feature!=="#securedoc")||state.decode_token?.Role==="user"?"":"cursor-pointer"} ${(state.memberAuthorization?.orgmbat_feature||state.leadAuthorization?.orgmbat_feature!=="#securedoc")||state.decode_token?.Role==="user"?"":"hover:bg-gray-200"} `}
+                onClick={state.decode_token?.Role === "user" ? Notallowed : sharedocumentRouter}
+                className={`font-semibold mr-0 lg:mr-4 my-2 text-center flex flex-col items-center justify-center w-48 h-48 px-6 py-4 border border-gray-300 rounded-lg  transition-colors duration-300 ${state.decode_token?.Role === "user" ? "bg-gray-100" : ""}   ${state.decode_token?.Role === "user" ? "" : "cursor-pointer"} ${state.decode_token?.Role === "user" ? "" : "hover:bg-gray-200"} `}
               >
                 <Image
                   src={UnderReview}
                   alt="logo"
-                  style={{ width: "70px", height: "75px",filter:state.decode_token?.Role==="user"?"grayscale(1)":"" }}
+                  style={{ width: "70px", height: "75px", filter: state.decode_token?.Role === "user" ? "grayscale(1)" : "" }}
                 />
                 <div className="my-3">
-                 Under
+                  Under
                   <br />
                   Review
                 </div>
               </div>
-          </div> 
+            ) : null}
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default Page;
+export default page;
--- app/ui/login/OTPverify/OTPverify.jsx
+++ app/ui/login/OTPverify/OTPverify.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useContext } from 'react'
-import VerificationCodeInput from './VerificationCodeInput'
+import VerificationCodeInput from './VerificationCodeotp'
 import { Box, Button } from '@mui/material';
 import handleotp from "@/handle/otpvelify"
 import handleqrcode from "@/handle/Authenticator"
@@ -11,31 +11,33 @@ function OTPverify() {
   const Handleresend = resend();
   const HandleOTP = handleotp();
   const HandleQRCode = handleqrcode();
-  const {state} = useContext(StateContext);
+  const { state } = useContext(StateContext);
   return (
-    <Box p={3} sx={{display: 'flex',flexDirection: 'column',background: 'white',width: '400px',height: '500px',
-    borderRadius: "15px",marginLeft: 'auto',mr: 5,mt: 1,boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',justifyContent:'space-between'}}>
     <Box>
-      <Box sx={{fontWeight:'800'}}>You are almost done!</Box> 
-      <br></br>
-      <Box>Please type the code we sent you to your email <strong>{state.email}</strong> and verify that it's really you.</Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', background: '#fff', width: '440px', height: '550px', borderRadius: "15px", marginLeft: 'auto', mr: 7, mt: 1, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', justifyContent: 'space-between', py: '35px', px: '25px' }}>
+        <Box sx={{ height: '100%', background: '', width: '90%', mx: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+          <Box sx={{ background: '', width: '100%', mx: 'auto' }}>
+            <Box sx={{ textAlign: 'left', color: '#1F2939', fontSize: 20, fontWeight: '700', mt: 1 }}>You are almost done!</Box>
+            <Box sx={{ textAlign: 'left', color: '#778296', fontSize: 15, mt: '8px' }}>Please type the code we sent you to your email and verify that it's really you.</Box>
+          </Box>
+          <Box>
+            <Box sx={{mb:'15px'}}>Code</Box>
+            <VerificationCodeInput onChange={HandleOTP.handleCodeChange} />
+            <Box sx={{ mt: '15px', display: 'flex', justifyContent: 'space-between' }}>
+              <Box sx={{ display: 'flex', gap: '10px' }}>
+                <p style={{ fontSize: '14px' }}>Reference No :</p>
+                <p style={{ fontSize: '14px', fontWeight: '600' }}>{state.referenceID}</p>
+              </Box>
+              <Box onClick={Handleresend.sendOTPEmail} sx={{ fontSize: '14px', fontWeight: '600', color: '#4D94FB', textTransform: 'capitalize', cursor: 'pointer', '&:hover':{color:'#1d39b0'} }} disabled={state.timer > 0} > {state.timer > 0 ? `Resend Code (${state.timer})` : "Resend Code"} </Box>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection:'column-reverse', gap:'10px'}}>
+            <Button variant="contained" onClick={HandleOTP.workspace} sx={{ transition: 'transform 0.3s ease', '&:hover': { transform: 'scale(1.03)', }, }} style={{ background: '#84BAA1', width: '100%', height: '44px', textTransform: 'capitalize', boxShadow: '0px 0px 0px', borderRadius: '8px', fontWeight: '600' }}>{state.loading ? (<Loading />) : "Next"}</Button>
+            <Button variant="text" sx={{ color: '#828895', textTransform: 'capitalize', fontSize: 14, fontWeight: '', '&:hover': { background: 'none', color: '#1F2939' }, '&:active': { background: 'none' } }} onClick={HandleQRCode.tryanother}>Try Another method</Button>
+          </Box>
+        </Box>
+      </Box>
     </Box>
-    <Box>
-    <Box>Code</Box>
-    <VerificationCodeInput onChange={HandleOTP.handleCodeChange} />
-    <Box sx={{mt:'10px',display:'flex',gap:'10px'}}>
-      <p style={{fontSize:'14px'}}>Reference No :</p>
-      <p style={{fontSize:'14px',fontWeight:'600'}}>{state.referenceID}</p>
-    </Box>
-    <Box sx={{display:'flex',justifyContent:'center',mt:'px'}}>
-    <Button onClick={Handleresend.sendOTPEmail} sx={{fontSize:'14px', fontWeight:'600',color:'#4D94FB',textTransform:'capitalize',transition:'color 0.3s'}} disabled={state.timer > 0} > {state.timer > 0 ? `Resend Code (${state.timer})` : "Resend Code"} </Button>
-    </Box>
-    </Box>
-    <Box sx={{display:'flex',justifyContent:'center'}}>
-    <Button variant="contained" onClick={HandleOTP.workspace} style={{background:'#84BAA1',width:'90%',textTransform:'capitalize'}}>{state.loading?(<Loading/>):"Next"}</Button>
-    </Box>
-    <Button variant="text" style={{textTransform:"capitalize",color:"gray"}} onClick={HandleQRCode.tryanother}>Try Another method</Button>
-  </Box>
   )
 }
 export default OTPverify
