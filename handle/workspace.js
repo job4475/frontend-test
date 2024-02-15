a53079e904b4f137bfe22fbb807bc5c1f12210cf
Pond161172 <kraiwit1711@gmail.com>
2024-02-15 13:21:38 +0700

    Disabled Button

--- handle/workspace.js
+++ handle/workspace.js
@@ -5,34 +5,31 @@ import { useContext } from 'react';
 
 function workspace() {
 
-    const {state,setState} = useContext(StateContext);
+    const { state, setState } = useContext(StateContext);
 
-        const router = useRouter();
+    const router = useRouter();
 
-        const handleToUserlist = () => {
+    const handleToUserlist = () => {
 
-            fetch("http://192.168.3.113:7777/api/admin/getAllMembers/c8122e61-6bc3-46af-82ed-3a4d5ae9560a")
-                .then((response) => response.json())
-                .then((result) => {
-                    if (result.status === 'OK') {
-                        setState({ ...state, userdata: result.data })
-                    }
-                })
-                .catch((error) => console.error(error));
-            router.push('/Userlist')
-        }
-
-        const handleclicklogout = () => {
-            localStorage.removeItem("ally-supports-cache")
-            localStorage.removeItem("decode_token")
-            localStorage.removeItem("loginTime")
-            window.location.href = "/"
-        }
-
-
-
-
-        return { handleToUserlist, handleclicklogout }
+        fetch('${process.env.NEXT_PUBLIC_API_ENDPOINT_GET}${process.env.NEXT_PUBLIC_API_PORT_LOGIN?`:${process.env.NEXT_PUBLIC_API_PORT_LOGIN}`:""}/api/admin/getAllMembers/c8122e61-6bc3-46af-82ed-3a4d5ae9560a')
+            .then((response) => response.json())
+            .then((result) => {
+                if (result.status === 'OK') {
+                    setState({ ...state, userdata: result.data })
+                }
+            })
+            .catch((error) => console.error(error));
+        router.push('/Userlist')
     }
+
+    const handleclicklogout = () => {
+        localStorage.removeItem("ally-supports-cache")
+        localStorage.removeItem("decode_token")
+        localStorage.removeItem("loginTime")
+        window.location.href = "/"
+    }
+
+    return { handleToUserlist, handleclicklogout }
+}
 
 export default workspace
