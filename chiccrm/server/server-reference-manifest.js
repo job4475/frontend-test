f023da96fb87a8d6ad289c4b547916e0b7a3806c
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2024-02-15 10:35:51 +0700

    disable confirm register

--- chiccrm/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3MjkNE67hjPNE7waj3Y2d52elh03B508w3PUHj07xSM=\"\n}"
--- jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    setupFilesAfterEnv: ['./configs/enzyme']
-    };
-
