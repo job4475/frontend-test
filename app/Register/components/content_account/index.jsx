110f0603cb6b9746dc51c20a4e35549dc131799b
wawayayoei1234 <you@example.com>
2024-02-16 17:32:36 +0700

    skel and colur text

--- app/Register/components/content_account/index.jsx
+++ app/Register/components/content_account/index.jsx
@@ -15,7 +15,7 @@ function Index() {
       ) : (<Skeleton variant="rectangular" width={100} height={100} style={{ borderRadius: '100px' }} />)}
       <Box sx={{ ml: 5 }}>
         {state.logoImage ? (
-          <Box sx={{ fontWeight: 600 }} dangerouslySetInnerHTML={{ __html: `Hello good morning ${state.email} <br/> Welcome to ChicCRM registration process now you are in` }} />
+           <Box sx={{  fontSize: '18px' }}>Hello good morning <b>{state.emailconfirm?state.emailconfirm: state.email}</b> <br></br>Welcome to ChicCRM registration process now you are in</Box>
         ) : (<Skeleton variant="rectangular" width={400} height={20} style={{ borderRadius: '6px', marginBottom:'10px' }} />)}
         {state.logoImage ? (
           <Box sx={{ fontSize: "12px", mt: 0.5 }} dangerouslySetInnerHTML={{ __html: data_register[0].detail_msg }} />
