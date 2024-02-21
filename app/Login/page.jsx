c7ec4b7103b1c6141f6217df0345d9d05c92dc91
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2024-02-21 20:53:10 +0700

    fix logo not show v.7.0.5

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -18,7 +18,7 @@ const Page = () => {
   return (
     <div className="flex flex-col xl:flex-row bg-[#F7FAFB] h-screen">
       <div className="basis-2/3 hidden xl:block">
-        {state.showContent?(
+        {!state.showContent?(
           <Image onClick={backToLogin}src={Chiclogo}alt="logo"className="absolute w-[80px] h-[auto] top-5 left-5 hover:scale-110 duration-500 cursor-pointer"/>
         ):(
           <Box m={3}>
