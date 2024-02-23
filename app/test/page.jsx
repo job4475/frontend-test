ae3223afad3e013375fd5c4caa65c1ac7065d001
wawayayoei1234 <you@example.com>
2024-02-23 09:17:49 +0700

    fix link so f**k long

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+'use client'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+import { Box } from '@mui/system'
+import Image from 'next/image'
+import React, { useContext } from 'react'
+import Subtract from '@/assets/assets/images/Subtract.png'
+import { StateContext } from '@/context/Context';
 
 function page() {
+  const { state, setState } = useContext(StateContext);
   return (
-    <div>page</div>
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 'auto', height: 'auto' }}>
+        <Image src={Subtract} alt="logo" style={{ width: "80px" }} />
+        <Box sx={{ pl: 3, width: 'auto', height: 'auto' }}>
+          <Box sx={{ fontSize: '15px' }}>Hello good morning <b>{state.emailconfirm ? state.emailconfirm : state.email}</b> <br />Welcome to ChicCRM registration process now you are in</Box>
+          <Box sx={{ fontSize: '10px' }}>After completing all information, you will receive an email with your password</Box>
+        </Box>
+      </Box>
   )
+
+
 }
 
 export default page
