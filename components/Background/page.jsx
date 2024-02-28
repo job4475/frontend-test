f5077d820de5ad3867c320e29ef393451cd74c23
Pond161172 <kraiwit1711@gmail.com>
2024-02-28 12:00:09 +0700

    Update Regiter disable Jobtitle

--- components/Background/page.jsx
+++ components/Background/page.jsx
@@ -18,15 +18,11 @@ const Layout = ({ children }) => {
     return (
         <Box display="flex" height="100vh" justifyContent="center" alignItems="center" bgcolor="#F7FAFB" px="70px" py="50px"  >
             <Box width="60%" height="100%" bgcolor="" display="flex" flexDirection="column" justifyContent="start" gap="20px" >
-                <Box>
                     <Image onClick={backToLogin} src={Chiclogo} alt="logo" className="w-[80px] h-[auto] hover:scale-110 duration-500 cursor-pointer" />
-                </Box>
-                <Box>
                     <Image src={Computer} style={{ width: '53%', maxWidth:'640px' , height: 'auto', marginLeft: 'auto', marginRight: 'auto' }} />
-                </Box>
             </Box>
 
-            <Box width="30%" maxWidth="500px" height="85%" px="42px" py="30px" bgcolor="#fff" borderRadius="17px" display="flex" flexDirection="column" justifyContent="space-between" boxShadow="0px 0px 50px 8px rgba(0,0,0,0.05)" >
+            <Box width="30%" maxWidth="500px" minWidth="400px" height="85%" maxHeight="550px" minHeight="480px" px="42px" py="30px" bgcolor="#fff" borderRadius="17px" display="flex" flexDirection="column" justifyContent="space-between" boxShadow="0px 0px 50px 8px rgba(0,0,0,0.05)" >
                 {children}
             </Box>
         </Box>
