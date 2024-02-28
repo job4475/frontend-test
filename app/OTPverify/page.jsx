03b79ff2c382cfa103ee001e55023c57c9fe3edc
wawayayoei1234 <you@example.com>
2024-02-28 08:44:35 +0700

    fix warning

--- app/OTPverify/page.jsx
+++ app/OTPverify/page.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 import Chiclogo from "@/assets/assets/images/sigin/chiclogo.png";
 
 const Page = () => {
-  const backlogin = () => {
+  const backToLogin = () => {
     window.location.href="/";
   }
   return (
     <div className="flex flex-col xl:flex-row bg-[#F7FAFB] h-screen">
       <div className="basis-2/3 hidden xl:block">
-      <Image onClick={backlogin} src={Chiclogo} alt="logo" className="absolute w-[80px] h-[auto] top-5 left-5 hover:scale-110 duration-500 cursor-pointer" />
+      <Image onClick={backToLogin}src={Chiclogo}alt="logo"className="absolute w-[80px] h-[auto] top-5 left-5 hover:scale-110 duration-500 cursor-pointer"priority/>
       </div>
       <div className="my-[auto] xl:mt-[auto] basis-1/1 xl:basis-1/3 flex flex-col content-center justify-center items-center">
         <OTPverify />
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,129 +1,108 @@
-'use client'
+"use client"
+import { Box, Button, Skeleton, TextField } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { StateContext } from '@/context/Context';
+import handlelogin from '@/handle/login'
+import Loading from '@/components/loading'
+import Backdrop from '@/components/backdrop/backdrop'
+import Dialog from '@/components/dialog/dialog'
+import React,{ useContext, useEffect, useState } from 'react';
 
-import React, { useState, useEffect } from 'react';
-import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-import axios from 'axios';
-import Image from 'next/image';
-import { Button } from '@mui/material';
-
-function App() {
-    const [ user, setUser ] = useState([]);
-    const [ profile, setProfile ] = useState([]);
-    const [ fileUploaded, setFileUploaded ] = useState(false);
-    const [file,setfile] = useState(null);
-    const [loading,setloading] = useState(false);
-
-    const login = useGoogleLogin({
-      onSuccess: (codeResponse) => setUser(codeResponse),
-      onError: (error) => console.log('Login Failed:', error),
-      accessType: 'offline', // เพิ่มบรรทัดนี้เพื่อร้องขอสิทธิ์ในการอัปโหลดไฟล์ไปยัง Google Drive
-      scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.appdata' // เพิ่มสิทธิ์ในการอัปโหลดไฟล์ไปยัง Google Drive
-  });
-  
-
-    useEffect(
-        () => {
-            if (user) {
-                console.log("🚀 ~ App ~ user:", user)
-                axios
-                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                        headers: {
-                            Authorization: `Bearer ${user.access_token}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                    .then((res) => {
-                        setProfile(res.data);
-                    })
-                    .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
-
-    const uploadFileToDrive = async () => {
-        setloading(true)
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", file ? file[0].type:"");
-        myHeaders.append("Authorization", `Bearer ${user.access_token}`);
-        
-        const files = file ? file[0]:"";
-        
-        const requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: files,
-          redirect: "follow"
-        };
-        
-        fetch("https://www.googleapis.com/upload/drive/v3/files?uploadType=media", requestOptions)
-          .then((response) => response.json())
-          .then((result) => 
-          {
-            Rename(result.id)
-          })
-          .catch((error) => console.error(error));
-    };
-
-    const Rename =(id)=>{
-      const myHeaders = new Headers();
-      myHeaders.append("Content-Type", "application/json");
-      myHeaders.append("Authorization", `Bearer ${user.access_token}`);
-      
-      const raw = JSON.stringify({
-        "title": file ? file[0].name:""
-      });
-      
-      const requestOptions = {
-        method: "PUT",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-      };
-      
-      fetch(`https://www.googleapis.com/drive/v2/files/${id?id:""}`, requestOptions)
-        .then((response) => response.json())
-        .then((result) => {
-          console.log("🚀 ~ .then ~ result:", result)
-          alert("Upload Successfully")
-          setloading(false)
-        })
-        .catch((error) => console.error(error));
+function Index() {
+  const { state, setState } = useContext(StateContext);
+  const [showContent, setShowContent] = useState(false);
+  const HandleLogin = handlelogin();
+  const handleEnterKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      state.info === 0 ? HandleLogin.handleSignInClick() : HandleLogin.handleSignUpClick();
     }
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-        setFileUploaded(false);
-    };
+  const onClickHandler = state.info === 0 ? HandleLogin.handleSignInClick : HandleLogin.handleSignUpClick;
+  const buttonText = state.loading ? <Loading /> : state.info === 0 ? 'Sign In' : 'Next';
 
-    return (
-        <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center"}}>
-            <h2>Upload files to Google drive</h2>
-            <br />
-            <br />
-            {profile ? (
-                <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center"}}>
-                    <Image width={100} height={100} src={profile?.picture} alt="user image" />
-                    <h3>User Logged in</h3>
-                    <p>Name: {profile.name}</p>
-                    <p>Email Address: {profile.email}</p>
-                    <br />
-                    <br />
-                    {!fileUploaded && (
-                      <div style={{display:"flex",justifyContent:"center",flexDirection:"row",alignItems:"center"}}>
-                        <input type='file' onChange={(e)=>{setfile(e.target.files)}}></input>
-                        <Button variant='contained' onClick={uploadFileToDrive}>{loading?"Loading...":"Upload File to Drive"}</Button>
-                        </div>
-                    )}
-                    <br />
-                    <br />
-                    <Button variant='contained' color="error" onClick={logOut}>Log out</Button>
-                </div>
-            ) : (
-                <Button variant='contained' color="success" onClick={login}>Sign in with Google 🚀 </Button>
-            )}
-        </div>
-    );
+  return (
+    <Box>
+      <Box sx={{
+        display: 'flex', flexDirection: 'column', background: '#fff', width: '440px', height: '550px',
+        borderRadius: "15px", marginLeft: 'auto', mr: 7, mt: 1, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', justifyContent: 'space-between', py: '35px', px: '25px'
+      }}>
+        {showContent ? (
+          <Box ml={2} sx={{
+            display: 'flex', flexDirection: 'row', width: '190px', height: '43px', borderRadius: '50px',
+            background: '#DAEBE3', alignItems: 'center', justifyContent: 'space-between', py: '3px', px: '4px'
+          }}>
+
+            <Box onClick={() => { setState({ ...state, info: 0 })}} sx={{
+              cursor: "pointer", width: "90px", height: "100%", flexShrink: 0,
+              background: state.info === 0 ? "#84BAA1" : "", color: state.info === 0 ? "#fff" : "#3d4d69", borderRadius: "40px", display: "flex",
+              justifyContent: "center", alignItems: "center", fontSize: '15px', fontWeight: '500'
+            }}>
+              <Box>Sign In</Box>
+            </Box>
+            <Box onClick={() => { setState({ ...state, info: 1 }) }} sx={{
+              cursor: "pointer", ml: -2, width: "90px", height: "100%", flexShrink: 0,
+              background: state.info === 1 ? "#84BAA1" : "", color: state.info === 1 ? "#fff" : "#3d4d69", borderRadius: "40px", display: "flex",
+              justifyContent: "center", alignItems: "center", fontSize: '15px', fontWeight: '500'
+            }}>
+              <Box>Sign Up</Box>
+            </Box>
+          </Box>
+        ) : (
+          <Skeleton variant="rectangular" width={210} height={45} style={{ borderRadius: '50px' }} />
+        )}
+        {showContent ? (<Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <TextField id="standard-basic" label="Email" variant="standard" value={state.email} onChange={HandleLogin.Email} onKeyDown={handleEnterKeyPress} sx={{ width: '90%', '& label': { color: '#828895', fontWeight: '500' } }} />
+          {state.info === 0 && (
+            <TextField label="Password" variant="standard" value={state.password} onChange={HandleLogin.Password} onKeyDown={handleEnterKeyPress} type={state.showPassword ? 'text' : 'password'} sx={{ width: '90%', mt: '15px', '& label': { color: '#828895', fontWeight: '500' } }}
+              InputProps={{
+                endAdornment: (
+                  <IconButton onClick={HandleLogin.handleTogglePassword} edge="end">
+                    {state.showPassword ? <VisibilityIcon sx={{ color: '#c1c7cd', mr: '5px' }} /> : <VisibilityOffIcon sx={{ color: '#c1c7cd', mr: '5px' }} />}
+                  </IconButton>
+                ),
+              }}
+            />
+          )}
+          {state.info === 0 && (
+            <Box sx={{ background: '', width: '95%', display: 'flex', justifyContent: 'end', mt: '5px' }}>
+              <Button variant="text" onClick={HandleLogin.ForgotPassword} sx={{ color: '#828895', textTransform: 'capitalize', '&:hover': { background: 'none', color: '#1F2939' }, '&:active': { background: 'none' } }}>Forgot Password?</Button>
+              {state.backdrop ? <Backdrop /> : ""}
+            </Box>
+          )}
+        </Box>):
+        ( <Box sx={{width:350}}>
+          <Skeleton variant="rectangular" width={350} height={45} style={{ borderRadius: '10px', marginBottom: '10px' }} />
+          <Skeleton variant="rectangular" width={350} height={45} style={{ borderRadius: '10px', marginBottom: '10px' }} />
+          <Box sx={{ }}>
+          <Skeleton variant="rectangular" width={150} height={25} style={{ borderRadius: '5px',marginLeft: 'auto' }} />
+          </Box>
+        </Box>
+        )
+        }
+         {showContent ? (
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button variant="contained" onClick={onClickHandler} sx={{ transition: 'transform 0.3s ease', '&:hover': { transform: 'scale(1.03)', }, }} style={{ background: state.loading ? '#e5e5e5' : '#84BAA1' , width: '90%', height: '44px', textTransform: 'capitalize', marginTop: 10, boxShadow: '0px 0px 0px', borderRadius: '8px', fontWeight: '600', cursor: state.loading ? 'not-allowed' : 'pointer',  }} disabled={state.loading}>
+             {buttonText}
+          </Button>
+          {state.open ? <Dialog /> : ""}
+        </Box>
+         ):(
+          <Skeleton variant="rectangular" width={350} height={45} style={{ borderRadius: '10px' }} />
+         )}
+        
+      </Box>
+    </Box>
+  );
 }
-export default App;
+
+export default Index;
