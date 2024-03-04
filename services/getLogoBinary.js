3e42d5d9c9f41bc476cb8a665b1909c0428fcd57
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2024-03-04 23:51:18 +0700

    fix image logo not show

--- services/getLogoBinary.js
+++ services/getLogoBinary.js
@@ -5,19 +5,27 @@ function GetLogoBinary() {
     const { state, setState } = useContext(StateContext);
 
     useEffect(() => {
-        if (state.decode_token.CompanyID||state.datacompanylc) {
+        if (state.decode_token.CompanyID || state.datacompanylc) {
             const apiEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT_GET;
             const apiPortLogin = process.env.NEXT_PUBLIC_API_PORT_LOGIN || "";
-            const apiUrl = `${apiEndpoint}:${apiPortLogin}/api/getLogoBinary/${state.decode_token.CompanyID?state.decode_token.CompanyID:state.datacompanylc.CompanyID}`;
+            const apiUrl = `${apiEndpoint}:${apiPortLogin}/api/getLogoBinary/${state.decode_token.CompanyID ? state.decode_token.CompanyID : state.datacompanylc.CompanyID}`;
+            
             fetch(apiUrl)
-                .then(response => response.blob())
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.blob();
+                })
                 .then(blob => {
                     const imageUrl = URL.createObjectURL(blob);
-                    setState((prevData) => ({ ...prevData, logoImage: imageUrl }));
+                    // Set state after API request is completed
+                    setState(prevData => ({ ...prevData, logoImage: imageUrl }));
                 })
                 .catch(error => console.error("Error fetching binary data:", error));
         }
-    }, [state.decode_token.CompanyID,state.allorder,state.allleadorder,state.datacompanylc]);
+    }, [state.decode_token.CompanyID, state.allorder, state.allleadorder, state.datacompanylc, setState]);
+    
 
     return null;
 }
