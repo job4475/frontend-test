66a85f7a985c16485ec3a67767cd27987a079be2
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2024-03-05 08:53:09 +0700

    add middleware test

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,27 +1,9 @@
-// 'use client'
- 
-// import { useState } from 'react'
-// import dynamic from 'next/dynamic'
- 
-// // Client Components:
-// const ComponentA = dynamic(() => import('../components/A'))
-// const ComponentB = dynamic(() => import('../components/B'))
-// const ComponentC = dynamic(() => import('../components/C'), { ssr: false })
- 
-// export default function ClientComponentExample() {
-//   const [showMore, setShowMore] = useState(false)
- 
-//   return (
-//     <div>
-//       {/* Load immediately, but in a separate client bundle */}
-//       <ComponentA />
- 
-//       {/* Load on demand, only when/if the condition is met */}
-//       {showMore && <ComponentB />}
-//       <button onClick={() => setShowMore(!showMore)}>Toggle</button>
- 
-//       {/* Load only on the client side */}
-//       <ComponentC />
-//     </div>
-//   )
-// }
+import React from 'react'
+
+function page() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
