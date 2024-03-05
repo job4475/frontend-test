5f50cbc79dc7af7a68886bb6053f592dd45d4b63
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2024-03-05 08:48:40 +0700

    add middleware changepassword2

--- app/dashboard/[slug]/page.js
+++
@@ -1,7 +0,0 @@
-export async function generateStaticParams() {
-  const posts = await fetch('http://.../posts').then((res) => res.json())
- 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }))
-}
