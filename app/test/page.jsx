88342e4b3aa5cbaf5ccefad27221765b504c2bf8
Pond161172 <kraiwit1711@gmail.com>
2024-03-26 09:58:28 +0700

    side bar

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,11 +1,86 @@
+"use client";
+import React, { useState } from 'react'
+import Arrow from '@mui/icons-material/KeyboardArrowLeftRounded';
+import Profile from '@mui/icons-material/AccountCircleRounded';
+import SpaceDashboardOutlinedIcon from '@mui/icons-material/SpaceDashboardOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import HubOutlinedIcon from '@mui/icons-material/HubOutlined';
+import PostAddOutlinedIcon from '@mui/icons-material/PostAddRounded';
+import SignalCellularAltRoundedIcon from '@mui/icons-material/SignalCellularAltRounded';
+import TaskAltRoundedIcon from '@mui/icons-material/TaskAltRounded';
+import CastRoundedIcon from '@mui/icons-material/CastRounded';
 
-import { Box } from '@mui/system'
-import React from 'react'
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { mainmenu: "Home" },
+    { mainmenu: "Notification", icon: <NotificationsNoneOutlinedIcon /> },
+    { mainmenu: "Administrator", icon: <AdminPanelSettingsOutlinedIcon /> },
+    { mainmenu: "Setting", icon: <SettingsOutlinedIcon /> },
+    { mainmenu: "Social Connect", icon: <HubOutlinedIcon /> },
+  ];
 
-function page() {
+  const workSpaceMenu = [
+    { mainmenu: "Share Documents" },
+    { mainmenu: "MyOpportunity", icon: <SignalCellularAltRoundedIcon /> },
+    { mainmenu: "Under Review", icon: <TaskAltRoundedIcon /> },
+    { mainmenu: "Remote Support", icon: <CastRoundedIcon /> },
+  ];
+
+
   return (
-    <Box>page</Box>
+    <div className='flex bg-[#f8fafc]'>
+      <div className={`bg-[#fff] h-screen px-4 py-6 relative duration-100 ${open ? "w-64" : "w-20"}`}>
+        <Arrow sx={{ fontSize: '35px' }} className={`text-[#000] absolute -right-9 top-1/2 cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+        <div className='inline-flex'>
+          <Profile sx={{ fontSize: '40px' }} className={`rounded-full block float-left mr-2 cursor-pointer`} />
+          <div className={`flex-col ${!open && "scale-0"}`}>
+            <h1 className={`text-[14px] origin-left font-semibold`}>Name Surname</h1>
+            <p className={`text-[12px] origin-left font-semibold`}>Job title</p>
+          </div>
+        </div>
+        <div className={`flex mt-3 ${!open && "scale-0"}`}>
+          <p className={`text-[10px] text-[#] font-medium uppercase`}>menu</p>
+        </div>
+        <ul>
+          {Menus.map((menu, index) => (
+            <>
+              <li key={index} className='text-[#1F2939] rounded-md text-sm font-medium flex items-center gap-x-3 cursor-pointer px-2 py-2.5 mt-1 hover:bg-[#1F2939] hover:text-[#fff] has-[:checked]:bg-indigo-50'>
+                <span sx={{ fontSize: '25px' }} className='block float-left'>
+                  {menu.icon ? menu.icon : <SpaceDashboardOutlinedIcon />}
+                </span>
+                <span className={`${!open && "hidden"}`}>
+                  {menu.mainmenu}
+                </span>
+              </li>
+            </>
+          ))}
+        </ul >
+        <div className={`flex mt-5 ${!open && "scale-0"}`}>
+          <p className={`text-[10px] text-[#] font-medium uppercase`}>work space</p>
+        </div>
+        <ul>
+          {workSpaceMenu.map((menu, index) => (
+            <>
+              <li key={index} className='text-[#1F2939] rounded-md text-sm font-medium flex items-center gap-x-3 cursor-pointer px-2 py-2.5 mt-1 hover:bg-[#1F2939] hover:text-[#fff]'>
+                <span sx={{ fontSize: '25px' }} className='block float-left'>
+                  {menu.icon ? menu.icon : <PostAddOutlinedIcon />}
+                </span>
+                <span className={`${!open && "hidden"}`}>
+                  {menu.mainmenu}
+                </span>
+              </li>
+            </>
+          ))}
+        </ul >
+      </div>
+      <div className='bg-[#fff] w-screen h-16 flex items-center px-7'>
+        <div className='text-xl text-[#1F2939] font-semibold'>Home</div>
+      </div>
+    </div>
   )
 }
 
-export default page
+export default Sidebar
